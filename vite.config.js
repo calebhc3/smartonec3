f13ee04a2c229b9e47db3a59d1f5ef35cb37af99
Matheus Martins <matheus.martins@c3saude.com.br>
2025-04-03 18:10:23 +0000

    Configs novo projeto

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+	host: '0.0.0.0',
+	port: 5175,
+	strictPort: true,
+	hmr: {
+	     host: '192.168.15.23',
+	     protocol: 'ws',
+	}
+     }
 });
