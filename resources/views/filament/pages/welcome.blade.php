1f78f79b9c187889274d4f106d985a7c20c7a12a
Matheus Martins <matheus.martins@c3saude.com.br>
2025-04-10 14:00:53 -0300

    logo

--- resources/views/filament/pages/welcome.blade.php
+++ resources/views/filament/pages/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="text-center">
         <h1 class="text-4xl font-bold text-gray-900 dark:text-white">
             @if (app(App\Filament\Pages\WelcomePage::class)->hasLogo())
-                <img src="{{ asset('path_to_logo/logo.png') }}" alt="Logo" class="w-40 h-40 mx-auto mb-4">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo" class="block h-9 w-auto" />
             @endif
             Seja bem-vinde ao sistema SmartOne C3
         </h1>
